fee51b260caee231384da663f436469a502e6dcf
malou <56136870+malougnom@users.noreply.github.com>
2019-10-26 09:38:04 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,4 +1,4 @@
-
+// Code here for main bot
 const Discord = require('discord.js');
 
 const client = new Discord.Client();
@@ -15,4 +15,15 @@ var prefix = '!'
      console.log('ready')
  })
 
+// This is to keep your bot online 24/7
+const express = require("express");
+var app = express();
+
+app.get("/", (req, res) => {
+  res.send({ data: "pong!" });
+});
+
+const listnere = app.listen(process.env.PORT, function() {
+  console.log("API Started! Port: " + process.env.PORT);
+});
 client.login(process.env.TOKEN)
